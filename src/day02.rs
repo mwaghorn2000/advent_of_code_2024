97b3a06d5e2ad28641aace082ca7e1505747fbb9
Mitchell Waghorn <z5513430@ad.unsw.edu.au>
2024-12-05 12:06:42 +1100

    created a dp solution for part 2 in O(nm)

--- src/day02.rs
+++ src/day02.rs
@@ -6,10 +6,10 @@ pub fn run(part: i32) {
 
     let reports = process_data(data);
 
-    let safe_reports_part_1 = 0;
-    let safe_reports_part_2 = 0;
+    let mut safe_reports_part_1 = 0;
+    let mut safe_reports_part_2 = 0;
 
-    generate_safe_reports(safe_reports_part_1: &i32, safe_reports_part_2: &i32, reports);
+    generate_safe_reports(&mut safe_reports_part_1, &mut safe_reports_part_2, reports);
 
     if part == 1 {
         println!("\n--- Day 2 ---\nPart 1: {}\n", safe_reports_part_1);
@@ -19,8 +19,8 @@ pub fn run(part: i32) {
 }
 
 fn generate_safe_reports(
-    safe_reports_part_1: &i32,
-    safe_reports_part_2: &i32,
+    safe_reports_part_1: &mut i32,
+    safe_reports_part_2: &mut i32,
     reports: Vec<Vec<i32>>
 ) {
     for report in reports {
@@ -29,25 +29,76 @@ fn generate_safe_reports(
         
         // dp initilisation
         let mut dp: Vec<Vec<bool>> = vec![vec![false; cols]; rows + 1];
+        // Track if at this point in the dp, are we increasing
+        let mut inc: Vec<Vec<bool>> = vec![vec![false; cols]; rows + 1];
+        // Track if at this point in the dp, are we decreasing
+        let mut dec: Vec<Vec<bool>> = vec![vec![false; cols]; rows + 1];
+        
+        // base cases - we set inc and dec to true since at the first element we can
+        // either increase of decrease of this element.
+        for i in 0..=1 {
+            for j in 0..=1 {
+                inc[i][j] = true;
+                dec[i][j] = true;
+                dp[i][j] = true;
+            }
+        }
 
-        // base cases
-        dp[0][0] = true;
-        dp[1][0] = true;
-        dp[0][1] = true;
-        dp[1][1] = true;
+        for i in 2..=rows {
+            if dp[i - 1][0] && is_valid(report[i - 2], report[i - 1], inc[i - 1][0], dec[i - 1][0]) {
+                inc[i][0] = is_increasing(report[i - 2], report[i - 1]);
+                dec[i][0] = !is_increasing(report[i - 2], report[i - 1]);
+                dp[i][0] = true;
+            }
 
-        for i in 1..rows {
-            
+            if dp[i][0] {
+                dp[i][1] = true;
+                inc[i][1] = inc[i][0];
+                dec[i][1] = dec[i][0];
+            }
+
+            if dp[i - 1][1] && is_valid(report[i - 2], report[i - 1], inc[i - 1][1], dec[i - 1][1]) {
+                dp[i][1] = true;
+                inc[i][1] = is_increasing(report[i - 2], report[i - 1]) || inc[i][1];
+                dec[i][1] = !is_increasing(report[i - 2], report[i - 1]) || dec[i][1];
+            }
+
+            // need to check if we are hitting our dummy value. In this case
+            // we are simulating removing the first element
+            if i == 2 {
+                dp[i][1] = true;
+                inc[i][1] = true;
+                dec[i][1] = true;
+            } else if dp[i - 2][0] && is_valid(report[i - 3], report[i - 1], inc[i - 2][0], dec[i - 2][0]) {
+                dp[i][1] = true;
+                inc[i][1] = is_increasing(report[i - 3], report[i - 1]) || inc[i][1];
+                dec[i][1] = !is_increasing(report[i - 3], report[i - 1]) || dec[i][1]; 
+            }
+        }
+
+        if dp[report.len()][0] {
+            *safe_reports_part_1 += 1;
+        } 
+        if dp[report.len()][1] || dp[report.len() - 1][0] {
+            *safe_reports_part_2 += 1;
         }
     }
 }
 
-fn check_increasing(value_1: i32, value_2: i32) -> bool {
-    value_1 < value_2
+fn is_valid(prev: i32, curr: i32, inc: bool, dec: bool) -> bool {
+    if inc && prev < curr && (curr - prev).abs() <= 3 {
+        return true
+    }
+
+    if dec && prev > curr && (prev - curr).abs() <= 3 {
+        return true
+    }
+
+    return false
 }
 
-fn check_safe(value_1: i32, value_2: i32) -> bool {
-    if 
+fn is_increasing(prev: i32, curr: i32) -> bool {
+    prev < curr
 }
 
 fn process_data(data: String) -> Vec<Vec<i32>> {
